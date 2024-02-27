e061d1edeca6843562f955570321b7d9f0f61d2b
Snorre Søvold <snorresovold@gmail.com>
2024-02-27 14:07:00 +0100

    remove react

--- src/functions/PDFGenerator.ts
+++ src/functions/PDFGenerator.ts
@@ -1,5 +1,19 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-import { exec } from "child_process";
+import { exec, spawn } from "child_process";
+
+function RunFiles () {
+    exec('ping google.com', (error, stdout, stderr) => {
+  
+        if (error) {
+          console.error(`error: ${error.message}`);
+          return;
+        }if (stderr) {
+          console.error(`stderr: ${stderr}`);
+          return;
+        }console.log(`stdout:\n${stdout}`);
+      }
+    );
+};
 
 export async function PDFGenerator(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
@@ -17,7 +31,7 @@ export async function PDFGenerator(request: HttpRequest, context: InvocationCont
         }
         context.log(`Stdout: ${stdout}`);
     });
-
+    RunFiles()
 
     return { body: `Hello, ${name}!` };
 };
