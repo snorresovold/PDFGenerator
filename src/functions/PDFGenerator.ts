1e6236a29c934312b0a61bf4f38476b7bceee982
Snorre Søvold <snorresovold@gmail.com>
2024-02-29 10:53:17 +0100

    works, slightly

--- src/functions/PDFGenerator.ts
+++ src/functions/PDFGenerator.ts
@@ -2,13 +2,34 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 import { exec, spawn } from "child_process";
 import { series } from 'async';
 
-function runCommand() {
-    series([
-      () => exec('cd web'),
-      () => exec('cd html-generator'),
-      () => spawn("npm run dev"),
-    ]);
-};
+function runCommand(){
+    const childProcess = spawn('npm', ['run', 'dev'], { cwd: 'src/html-generator' });
+
+    childProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    childProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    childProcess.on('error', (error) => {
+        console.error(`spawn error: ${error.message}`);
+    });
+
+    childProcess.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+
+    exec('cd src/PDFGenerator && node dist/main.js', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      });
+}
 
 export async function PDFGenerator(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
