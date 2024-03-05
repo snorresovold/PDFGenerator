04fa7c58863e682a18a490c8de92f9fbd0d6193e
Snorre Søvold <snorresovold@gmail.com>
2024-03-05 10:14:17 +0100

    midlertidig stopp

--- src/functions/PDFGenerator.ts
+++ src/functions/PDFGenerator.ts
@@ -1,12 +1,11 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { exec, spawn } from "child_process";
-import { series } from 'async';
 import * as fs from 'fs';
 import path = require("node:path");
 
 function runCommand(){
     const childProcess = spawn('npm', ['run', 'preview'], { cwd: 'src/html-generator' });
-
+    
     childProcess.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
     });
@@ -31,37 +30,47 @@ function runCommand(){
         console.error(`stderr: ${stderr}`);
       });
     childProcess.kill();
+
+    const PDF = fs.readFile("./src/PDFGenerator/output.pdf", (err, data) => {
+        if (err) {
+            console.error("Error reading the file:", err);
+            return;
+        }
+        console.log(data);
+    });
+    return PDF
 }
 
 export async function PDFGenerator(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
-    fs.readdir(path.join(process.cwd(), './src/PDFGenerator'), (err, files) => {
-        if (err) {
-            console.error('Error reading directory:', err);
-            return;
-        }
-    const output = "./src/PDFGenerator/output.pdf"
-    console.log(files);
-    });
+
+    const data = request.query.get('data') || (await request.text()) || 'world';
+    console.log(data);
     try {
-        const outputPath = '/Users/snorresovold/Documents/GitHub/PDFGenerator/src/PDFGenerator/ouput.pdf';
-        const body: any = await request.json();
+        const files = fs.readdirSync(path.join(process.cwd(), './src/PDFGenerator'));
+        console.log(files);
+    } catch (err) {
+        console.error('Error reading directory:', err);
+    }
+    const outputPath = './src/PDFGenerator/output.pdf';
+    try {
         const pdfBuffer = fs.readFileSync(outputPath);
-        console.log(pdfBuffer, "Test");
         return {
             body: pdfBuffer,
             status: 200,
             headers: {
                 'Content-Type': 'application/pdf',
-                'Content-Disposition': `attachment; filename="${outputPath}.pdf"`
-            }}
-    } catch {
+                'Content-Disposition': `attachment; filename="output.pdf"`
+            }
+        };
+    } catch (err) {
+        console.error('Error reading PDF file:', err);
         return {
-            body: "Bad Request, try giving an ISO-8859-1 encoded html string",
-            status: 400,
-        }
+            status: 500,
+            body: 'Internal Server Error'
+        };
     }
-};
+}
 
 app.http('PDFGenerator', {
     methods: ['GET', 'POST'],
