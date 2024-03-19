37d16cb13f222b90a6ced8154aaf0ba3f3661fa6
Snorre Søvold <snorresovold@gmail.com>
2024-03-19 14:06:46 +0100

    updates for tday

--- src/html-generator/src/KundeSignatur.jsx
+++ src/html-generator/src/KundeSignatur.jsx
@@ -1,12 +1,22 @@
 function KundeSignatur(props) {
+
+  function removeFirstAndLast(str) {
+    if (str.length >= 2) {
+        return str.substring(1, str.length - 1);
+    } else {
+        return str;
+    }
+  }
+
   return (
     <div className="dataContainer">
-        <p className="headerText">Signatur Kunde</p>
-        <p>Navn: {props.navn}</p>
-        <p>Dato: {props.dato}</p>
-        <p>Signatur: {props.signatur}</p>
+      <p className="headerText">Signatur Kunde</p>
+      <p>Navn: {props.navn}</p>
+      <p>Dato: {props.dato}</p>
+      <p>Signatur:</p>
+      <img src={removeFirstAndLast(props.signatur)} />
     </div>
   )
 }
 
-export default KundeSignatur
+export default KundeSignatur;
--- src/html-generator/src/components/TKSSignatur.jsx
+++ src/html-generator/src/components/TKSSignatur.jsx
@@ -1,10 +1,20 @@
 function TKSSignatur(props) {
-  return (
+
+  function removeFirstAndLast(str) {
+    if (str.length >= 2) {
+        return str.substring(1, str.length - 1);
+    } else {
+        return str;
+    }
+  }
+  
+return (
     <div className="dataContainer">
       <p className="headerText">Signatur TKS Heis</p>
       <p>Navn: {props.navn}</p>
       <p>Dato: {props.dato}</p>
-      <p>Signatur: {props.signatur}</p>
+      <p>Signatur: </p>
+      <img src={removeFirstAndLast(props.signatur)}/>
     </div>
   );
 }
