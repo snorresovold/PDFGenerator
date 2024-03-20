4be4be643e56821255ef18f080fba22ec4e27337
Snorre Søvold <snorresovold@gmail.com>
2024-03-20 14:58:50 +0100

    conditional rendering

--- src/html-generator/src/App.jsx
+++ src/html-generator/src/App.jsx
@@ -11,19 +11,24 @@ import OppfolgingsTable from "./components/OppfolgingsTable";
 import data from "./data/data.json";
 
 function App() {
-  return (
-    <div>
-      <h1>Ettersyn Personheis</h1>
-      <KundeInformasjon {...data.KundeInformasjon} />
-      <AnleggsInformasjon {...data.AnleggsInformasjon} />
-      <OppdragsInformasjon {...data.OppdragsInformasjon} />
-      <UtførtTable tasks={data.UtførtTable.tasks} />
-      <OppfolgingsTable followUps={data.OppfolgingsTable.followUps} />
-      <FakturaKommentar {...data.FakturaKommentar} />
-      <TKSSignatur {...data.TKSSignatur} />
-      <KundeSignatur {...data.KundeSignatur} />
-      <SigneringsNotat {...data.SigneringsNotat} />
-    </div>
-  );
+  try {
+    return (
+      <div>
+        <h1>Ettersyn Personheis</h1>
+          {data.KundeInformasjon && <KundeInformasjon {...data.KundeInformasjon} />}
+          {data.AnleggsInformasjon && <AnleggsInformasjon {...data.AnleggsInformasjon} />}
+          {data.OppdragsInformasjon && <OppdragsInformasjon {...data.OppdragsInformasjon} />}
+          {data.UtførtTable && <UtførtTable tasks={data.UtførtTable.tasks} />}
+          {data.OppfolgingsTable && <OppfolgingsTable followUps={data.OppfolgingsTable.followUps} />}
+          {data.FakturaKommentar && <FakturaKommentar {...data.FakturaKommentar} />}
+          {data.TKSSignatur && <TKSSignatur {...data.TKSSignatur} />}
+          {data.KundeSignatur && <KundeSignatur {...data.KundeSignatur} />}
+          {data.SigneringsNotat && <SigneringsNotat {...data.SigneringsNotat} />}
+      </div>
+    );
+  } catch {
+    return (<h1>wrong json format</h1>)
+  }
+
 }
 export default App;
